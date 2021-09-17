467225f15340dfd3829d3c4994435d4a516d664d
HolyDarknessMystic721 <70783250+HolyDarknessMystic721@users.noreply.github.com>
2021-09-17 08:41:16 -0400

    Moving the tree and making it change directions.

---
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+
+    //Prefab for instantiating apples
+    public GameObject applePrefab;
+
+    //Speed that the AppleTree moves
+    public float speed = 1f;
+
+    //Distance where AppleTree turns around
+    public float leftAndRightEdge = 10f;
+
+    //chance that the AppleTree will change directions
+    public float chanceToChangeDirections = 0.1f;
+
+    //Rate at which Apples will be instantiated
+    public float secondsBetweenAppleDrops = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+        //Needs apples dropping every second
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() { 
+        // Below is for basic movement.
+        Vector3 pos = transform.position; 
+        pos.x += speed * Time.deltaTime; 
+        transform.position = pos;
+
+        /*Below is for changing directions randomly. 
+        The first one is to move to the right. 
+        The second one is to move to the left.*/
+        if (pos.x < -leftAndRightEdge) { 
+        speed = Mathf.Abs(speed);
+
+        } else if (pos.x > leftAndRightEdge) { 
+            speed = -Mathf.Abs(speed);
+        }
+
+    }
+    void FixedUpdate() {
+        //To change directions randomly is time-based
+        if (Random.value < chanceToChangeDirections) {
+            speed *= -1;
+        }
+    }
+
+}
