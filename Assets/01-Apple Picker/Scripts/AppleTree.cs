a51d53ceca603db931f285e1e7e9d84e93873a6b
HolyDarknessMystic721 <70783250+HolyDarknessMystic721@users.noreply.github.com>
2021-09-17 08:48:54 -0400

    Dropping Apples and making sure the tree does not fly into the sun.

--- Assets/01-Apple Picker/Scripts/AppleTree.cs
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -22,10 +22,14 @@ public class AppleTree : MonoBehaviour
     public float secondsBetweenAppleDrops = 1f;
 
     // Start is called before the first frame update
-    void Start()
+    void Start() {
         //Needs apples dropping every second
-    {
-        
+        Invoke ("DropApple", 2f);
+    }
+    void DropApple() {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke ("DropApple", secondsBetweenAppleDrops);
     }
 
     // Update is called once per frame
